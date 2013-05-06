24356a992ecf29b9d5f4fe642ce244174c827d99
Dan Bennett <naebeth@googlemail.com>
2013-05-06 16:01:43 +0200

    initial commit

---
+++ greyout.js
@@ -0,0 +1,156 @@
+var greyout = function(opts){
+	if(opts !== undefined && typeof opts !== 'object') return false;
+	var _grey = {};
+	_grey.jQuery = (this !== window);
+	_grey.find = ((typeof opts === 'undefined') ? true : ((typeof opts.ids === 'undefined') ? true : false ));
+	_grey.act = ((typeof opts !== 'undefined') ? ((typeof opts.act === 'undefined') ? 'disable' : opts.act ) : 'disable');
+
+	_grey.parseAttribs = function(elem){
+		_grey.elems = _grey.elems || {};
+		elem = ((typeof elem === 'object') ? elem : ((_grey.jQuery) ? jQuery('#'+elem) : document.getElementById(elem)));
+		// console.log(elem);
+		for(i = 0, a = elem.attributes, l = a.length; i < l; i++){
+			attrib = a[i];
+			if(typeof attrib.nodeName !== 'undefined' && attrib.nodeName.indexOf('data-greyout-') >= 0){
+				_name = attrib.nodeName.substr(13);
+				switch(_name){
+					case 'type':
+						if(attrib.nodeValue === 'controller'){
+							_grey.elems[elem.id] = ((typeof _grey.elems[elem.id] === 'undefined') ? {name: elem.id, minions: []} : _grey.elems[elem.id]);
+							// console.log(_grey.elems);
+						}
+						break;
+					case 'controller':
+						if(typeof _grey.elems[attrib.nodeValue] === 'object'){
+							_grey.elems[attrib.nodeValue].minions.push(elem.id);
+						} else {
+							_grey.elems[attrib.nodeValue] = {name: attrib.nodeValue, minions: [elem.id]};
+						}
+						break;
+				}
+			}
+		}
+	};
+	_grey.keyup = function(e){
+		if(e.target.value.length > 0){
+			for(el in _grey.elems){
+				if(el !== e.target.id){
+					el = _grey.elems[el];
+					_grey.hider(el.name);
+					for(i = 0, l = el.minions.length; i < l; i++){
+						_grey.hider(el.minions[i]);
+					}
+				}
+			}
+		} else {
+			for(el in _grey.elems){
+				if(el !== e.target.id){
+					el = _grey.elems[el];
+					_grey.shower(el.name);
+					for(i = 0, l = el.minions.length; i < l; i++){
+						_grey.shower(el.minions[i]);
+					}
+				}
+			}
+		}
+	};
+	_grey.hider = function(elem){
+		elem = ((_grey.jQuery) ? '#'+elem : elem);
+		if(_grey.jQuery){
+			if(jQuery(elem).val().length > 0){
+				jQuery(elem).attr('data-greyout-oldval', jQuery(elem).val());
+				jQuery(elem).val(null);
+			}
+		} else {
+			_el = document.getElementById(elem);
+			if(_el.value.length > 0){
+				_el.attributes['data-greyout-oldval'] = _el.value;
+				_el.value = null;
+			}
+		}
+		switch(_grey.act){
+			case 'hide':
+				if(_grey.jQuery && jQuery.hide){
+					jQuery(elem).hide();
+				}
+			case 'disable':
+			default:
+				if(_grey.jQuery){
+					jQuery(elem).attr('disabled', 'disabled');
+				} else {
+					_el.attributes['disabled'] = 'disabled';
+				}
+				break;
+		}
+	};
+	_grey.shower = function(elem){
+		elem = ((_grey.jQuery) ? '#'+elem : elem);
+		if(_grey.jQuery){
+			if(typeof jQuery(elem).attr('data-greyout-oldval') !== 'undefined'){
+				jQuery(elem).val(jQuery(elem).attr('data-greyout-oldval'));
+				jQuery(elem).removeAttr('data-greyout-oldval');
+			}
+		} else {
+			_el = document.getElementById(elem);
+			if(_el.attributes['data-greyout-oldval'].length > 0){
+				_el.value = _el.attributes['data-greyout-oldval'];
+				_el.attributes['data-greyout-oldval'] = null;
+			}
+		}
+		switch(_grey.act){
+			case 'hide':
+				if(_grey.jQuery && jQuery.show){
+					jQuery(elem).show();
+				}
+			case 'disable':
+			default:
+				if(_grey.jQuery){
+					jQuery(elem).removeAttr('disabled');
+				} else {
+					_el.attributes['disabled'] = null;
+				}
+				break;
+		}
+	};
+
+	if(_grey.find){
+		if(_grey.jQuery){
+			jQuery.each(jQuery('input[type="text"]'), function(i, v){
+				_grey.parseAttribs(v);
+			});
+		} else {
+			elems = document.getElementsByTagName('input');
+			// console.log(elems.length);
+			for(i = 0, l = elems.length; i < l; i++){
+				// console.log(i, l, elems[i]);
+				if(typeof elems[i].attributes !== 'undefined' && elems[i].attributes.type.nodeValue.toLowerCase() === 'text'){
+					_grey.parseAttribs(elems[i]);
+				}
+			}
+		}
+	} else {
+		_grey.elems = opts.ids;
+	}
+
+	for(elem in _grey.elems){
+		elem = _grey.elems[elem];
+		if(typeof elem.name !== 'undefined'){
+			if(_grey.jQuery){
+				jQuery('#'+elem.name).on('keyup', _grey.keyup);
+			} else {
+				_el = document.getElementById(elem.name);
+				_el.onkeyup = _grey.keyup;
+			}
+		}
+	}
+
+	this._grey = _grey;
+}
+
+if(typeof jQuery !== 'undefined'){
+	(function($){
+		// $.fn.greyout = greyout;
+		$.greyout = greyout;
+		greyout = null;
+	})(jQuery);
+}
