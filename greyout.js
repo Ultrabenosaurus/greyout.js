0cc0e93b306aee0e85027de49178243ace28e996
Dan Bennett <naebeth@googlemail.com>
2013-05-07 16:27:37 +0200

    redesigned the concept of controllers
    assignment of control elements is much more fluid
    works both via data attribute and passing an object

--- greyout.js
+++ greyout.js
@@ -2,52 +2,49 @@ var greyout = function(opts){
 	if(this === window) return false;
 	if(opts !== undefined && typeof opts !== 'object') return false;
 	var _grey = {};
-	_grey.find = ((typeof opts === 'undefined') ? true : ((typeof opts.ids === 'undefined') ? true : false ));
+	_grey.find = ((typeof opts === 'undefined') ? true : ((typeof opts.hierarchy === 'undefined') ? true : false ));
 	_grey.act = ((typeof opts !== 'undefined') ? ((typeof opts.act === 'undefined') ? 'disable' : opts.act ) : 'disable');
+	_grey.logging = ((typeof opts !== 'undefined') ? ((typeof opts.logging === 'undefined') ? 0 : ((typeof opts.logging === 'number' && opts.logging >= 0) ? opts.logging : 0) ) : 0);
 
+	_grey.logger = function(){
+		if(typeof console === 'undefined' || typeof console.log === 'undefined') return false;
+		if(arguments.length < 1) return false;
+		arguments = Array.prototype.slice.call(arguments);
+		if(arguments.length < 2) arguments.push(1);
+
+		if(arguments.pop() <= _grey.logging){
+			console.log.apply(console, arguments);
+			if(typeof console.trace !== 'undefined'){
+				console.trace();
+			}
+		}
+	};
 	_grey.parseAttribs = function(elem){
 		_grey.elems = _grey.elems || {};
-		elem = ((typeof elem === 'object') ? jQuery(elem)[0] : jQuery('#'+elem)[0]);
-		for(i = 0, a = elem.attributes, l = a.length; i < l; i++){
-			attrib = a[i];
-			if(typeof attrib.nodeName !== 'undefined' && attrib.nodeName.indexOf('data-greyout-') >= 0){
-				_name = attrib.nodeName.substr(13);
-				switch(_name){
-					case 'type':
-						if(attrib.nodeValue === 'controller'){
-							_grey.elems[elem.id] = ((typeof _grey.elems[elem.id] === 'undefined') ? {name: elem.id, minions: []} : _grey.elems[elem.id]);
-						}
-						break;
-					case 'controller':
-						if(typeof _grey.elems[attrib.nodeValue] === 'object'){
-							_grey.elems[attrib.nodeValue].minions.push(elem.id);
-						} else {
-							_grey.elems[attrib.nodeValue] = {name: attrib.nodeValue, minions: [elem.id]};
-						}
-						break;
+		elem = ((typeof elem === 'object') ? jQuery(elem) : jQuery('#'+elem));
+		_contr = ((typeof elem.data('greyout-controllers') === 'undefined') ? false : elem.data('greyout-controllers').split(','));
+		if(!_contr) return false;
+
+		for(i = 0, l = _contr.length; i < l; i++){
+			_el = ((typeof jQuery("#"+_contr[i])[0] === 'undefined') ? false : jQuery("#"+_contr[i]));
+			if(_el){
+				if(typeof _grey.elems[elem[0].id] === 'undefined'){
+					_grey.elems[elem[0].id] = {name: elem[0].id, controllers: [_contr[i]]};
+				} else {
+					_grey.elems[elem[0].id].controllers.push(_contr[i]);
 				}
 			}
 		}
 	};
 	_grey.keyup = function(e){
-		if(e.target.value.length > 0){
-			for(el in _grey.elems){
-				if(el !== e.target.id){
-					el = _grey.elems[el];
-					_grey.hider(el.name);
-					for(i = 0, l = el.minions.length; i < l; i++){
-						_grey.hider(el.minions[i]);
-					}
-				}
-			}
-		} else {
-			for(el in _grey.elems){
-				if(el !== e.target.id){
-					el = _grey.elems[el];
-					_grey.shower(el.name);
-					for(i = 0, l = el.minions.length; i < l; i++){
-						_grey.shower(el.minions[i]);
-					}
+		_elem = jQuery("#"+this.id);
+		for(el in _grey.elems){
+			_contr = _grey.elems[el].controllers;
+			if(_contr.indexOf(this.id) >= 0){
+				if(e.target.value.length > 0){
+					_grey.hider(_grey.elems[el].name);
+				} else {
+					_grey.shower(_grey.elems[el].name);
 				}
 			}
 		}
@@ -88,21 +85,22 @@ var greyout = function(opts){
 	};
 
 	if(_grey.find){
-		jQuery.each(jQuery('input[type="text"]'), function(i, v){
+		jQuery.each(jQuery('input'), function(i, v){
 			_grey.parseAttribs(v);
 		});
 	} else {
-		_grey.elems = opts.ids;
+		_grey.elems = opts.hierarchy;
 	}
-
-	for(elem in _grey.elems){
-		elem = _grey.elems[elem];
-		if(typeof elem.name !== 'undefined'){
-			jQuery('#'+elem.name).on('keyup', _grey.keyup);
+	
+	for(el in _grey.elems){
+		_contr = _grey.elems[el].controllers;
+		for(_elem in _contr){
+			jQuery('#'+_contr[_elem]).on('keyup', _grey.keyup);
 		}
 	}
 
 	this._grey = _grey;
+	return this;
 }
 
 if(typeof jQuery !== 'undefined'){
